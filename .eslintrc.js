dcc6724785631e1150fcffd285eeafa71cfa9b2b
hcl-z <60871037+hcl-z@users.noreply.github.com>
2021-05-13 16:06:45 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     node: true
   },
   rules: {
-    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-explicit-any': 1,
     '@typescript-eslint/no-empty-function': 0,
     '@typescript-eslint/ban-types': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0
